0c45401827d246689f034ac78533157eee3a31d2
Marcin Kowalczyk <k123443@outlook.com>
2017-10-05 12:48:30 +0200

    Tcl update

--- build.tcl
+++ build.tcl
@@ -119,12 +119,12 @@ set_property "ip_repo_paths" "[file normalize "$origin_dir/SVC_test.ip_repo"]" $
 update_ip_catalog -rebuild
 
 # Set 'sources_1' fileset object
-set obj [get_filesets sources_1]
-set files [list \
- "[file normalize "$origin_dir/SVC_test.srcs/sources_1/bd/main_block_design/main_block_design.bd"]"\
- "[file normalize "$origin_dir/SVC_test.srcs/sources_1/bd/main_block_design/hdl/main_block_design_wrapper.v"]"\
-]
-add_files -norecurse -fileset $obj $files
+#set obj [get_filesets sources_1]
+#set files [list \
+# "[file normalize "$origin_dir/SVC_test.srcs/sources_1/bd/main_block_design/main_block_design.bd"]"\
+# "[file normalize "$origin_dir/SVC_test.srcs/sources_1/bd/main_block_design/hdl/main_block_design_wrapper.v"]"\
+#]
+#add_files -norecurse -fileset $obj $files
 
 # Set 'sources_1' fileset file properties for remote files
 # None
